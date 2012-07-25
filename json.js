13e8d24889b4dddf7d244b6bc0ac6c3ece263922
Abrham Kowitz <stormsurfin@fullsail.edu>
2012-07-25 16:43:52 -0600

    fourth project added to repo

---
+++ json.js
@@ -0,0 +1,38 @@
+var json = {
+	"record1":{
+		"familySelect": ["Side of Family:", "Immediate"],
+		"firstName": ["First Name:", "Abraham"],
+		"lastName": ["Last Name:", "Kowitz"],
+		"age": ["Age:", "32"],
+		"sex": ["Sex:", "Male"],
+		"eatHabits": ["Eating Habits:", "Much healthier than most"],
+		"exerciseHabits": ["Exercising Habits:", "Much more often than most"],
+		"diabetes": ["Diabetes:", "No"],
+		"docVisit": ["Last Checkup:", "2011-11-11"],
+		"comments": ["Doctor's Comments:", "Healthy"]
+	},
+	"record2":{
+		"familySelect": ["Side of Family:", "Immediate"],
+		"firstName": ["First Name:", "Matthew"],
+		"lastName": ["Last Name:", "Kowitz"],
+		"age": ["Age:", "32"],
+		"sex": ["Sex:", "Male"],
+		"eatHabits": ["Eating Habits:", "Much healthier than most"],
+		"exerciseHabits": ["Exercising Habits:", "Much more often than most"],
+		"diabetes": ["Diabetes:", "No"],
+		"docVisit": ["Last Checkup:", "2010-11-11"],
+		"comments": ["Doctor's Comments:", "Healthy"]
+	},
+	"record3":{
+		"familySelect": ["Side of Family:", "Immediate"],
+		"firstName": ["First Name:", "Wanda"],
+		"lastName": ["Last Name:", "Kowitz"],
+		"age": ["Age:", "65"],
+		"sex": ["Sex:", "Male"],
+		"eatHabits": ["Eating Habits:", "Average"],
+		"exerciseHabits": ["Exercising Habits:", "Average"],
+		"diabetes": ["Diabetes:", "No"],
+		"docVisit": ["Last Checkup:", "2012-11-11"],
+		"comments": ["Doctor's Comments:", "Old"]
+	}
+}
